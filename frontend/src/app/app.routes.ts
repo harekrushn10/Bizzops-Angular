e547a0a616521697dc3141ee0d6aa86fee9c6a44
Shyamkadiwar <shyamkadiwar17@gmail.com>
2025-03-05 18:53:51 +0530

    added invoice

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { InventoryComponent } from './components/inventory/inventory.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SalesComponent } from './components/sales/sales.component';
+import { InvoiceComponent } from './components/invoice/invoice.component';
 
 export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'inventory', component: InventoryComponent },
   { path: 'sales', component: SalesComponent },
-//   { path: 'invoice', component: InvoiceComponent },
+  { path: 'invoice', component: InvoiceComponent },
 //   { path: 'expenses', component: ExpensesComponent },
 //   { path: 'payments', component: PaymentsComponent },
 //   { path: 'orders', component: OrdersComponent },
